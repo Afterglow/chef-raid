094bfeea55a1a5e6545e0fb05886e874b4666c52
Paul Thomas <paul.thomas@livingsocial.com>
2012-08-23 15:57:14 +0100

    play with renaming resources

---
+++ resources/logical_span.rb
@@ -0,0 +1,6 @@
+actions :createld, :delete, :clear
+
+attribute :controller, :kind_of => String
+attribute :ld, :kind_of => String
+attribute :pds, :kind_of => Array
+attribute :level, :kind_of => String
