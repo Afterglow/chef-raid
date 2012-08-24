1d1b3bf3c4beb1244e2d07d1629b2af7a89a87b5
Paul Thomas <paul.thomas@livingsocial.com>
2012-08-24 11:53:44 +0100

    add new raid::manage recipe, insert informative comment in default.rb

--- recipes/manage.rb
+++ recipes/manage.rb
@@ -1,6 +1,6 @@
 #
 # Cookbook Name:: raid
-# Recipe:: default
+# Recipe:: manage
 #
 # Copyright 2012, Paul Thomas
 #
@@ -17,42 +17,25 @@
 # limitations under the License.
 
 #
-# This recipe only contains examples. I thought it unwise to have a default.rb
+# This recipe is in place of default.rb I thought it unwise to have a default.rb
 # that cleared peoples RAID configurations out the box. You have to implicitly
-# include the raid::manage recipe before the attributes set on your node will
+# include this raid::manage recipe before the attributes set on your node will
 # be applied. It is worth noting the configuration is cleared and rewritten on every
 # run, therefore you probably don't want this in your default run list. I use it from
 # solo for a single shot configuration on a new server.
 #
 
-#
-## Some examples
-#
-## Clear the configuration
-#raid_controller "0" do
-#  action :clear
-#  provider "raid_megaraid"
-#end
+# Initially clear the configuration
+include_recipe "raid::clear"
 
-## Delete a logical drive
-#raid_controller "0" do
-#  ld "0"
-#  action :delete
-#  provider "raid_megaraid"
-#end
-
-## Create a logical drive
-#raid_controller "0" do
-#  level "0"
-#  pds ["6:0","6:1"]
-#  action :createld
-#  provider "raid_megaraid"
-#end
-
-## Create a logical span
-#raid_controller "0" do
-#  level "10"
-#  pds [["6:0","6:1"],["6:2","6:3"]]
-#  action :createspan
-#  provider "raid_megaraid"
-#end
+node['raid']['logicaldrives'].each do |ld|
+  raid_controller ld['controller'] do
+    level ld['raidlevel']
+    pds ld['drives']
+    if pds[0].kind_of?(Array)
+      action :createspan
+    else
+      action :createld
+    end
+  end
+end
